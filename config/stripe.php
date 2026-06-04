693036f612af2ae9b1714e8cd9291edff758afaf
in2test <116454815+in2test@users.noreply.github.com>
2026-06-04 07:09:41 +0200

    updated to stripe new API

--- config/stripe.php
+++ config/stripe.php
@@ -1,10 +1,8 @@
 <?php
-
 declare(strict_types=1);
-
 return [
     'key' => env('STRIPE_KEY'),
     'secret' => env('STRIPE_SECRET'),
     'webhook_secret' => env('STRIPE_WEBHOOK_SECRET'),
-    'api_version' => '2026-04-22.dahlia',
+    'api_version' => '2026-05-27.dahlia',
 ];
