0f9a759f96de8bc1184493e18be3e832cd0fdd8e
Alin Office <deepinart@gmail.com>
2026-06-15 18:33:34 +0200

    debug: wrap feed index in try-catch to print trace

--- app/Http/Controllers/GoogleMerchantFeedController.php
+++ app/Http/Controllers/GoogleMerchantFeedController.php
@@ -13,52 +13,57 @@ class GoogleMerchantFeedController extends Controller
 {
     public function index(): Response
     {
-        $products = Product::where('is_active', true)
-            ->with(['media'])
-            ->get();
+        try {
+            $products = Product::where('is_active', true)
+                ->with(['media'])
+                ->get();
 
-        $xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><rss xmlns:g="http://base.google.com/ns/1.0" version="2.0"></rss>');
-        $channel = $xml->addChild('channel');
-        $channel->addChild('title', config('app.name', 'Pubblicittà24'));
-        $channel->addChild('link', url('/'));
-        $channel->addChild('description', 'Catalogo Prodotti '.config('app.name', 'Pubblicittà24'));
+            $xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><rss xmlns:g="http://base.google.com/ns/1.0" version="2.0"></rss>');
+            $channel = $xml->addChild('channel');
+            $channel->addChild('title', config('app.name', 'Pubblicittà24'));
+            $channel->addChild('link', url('/'));
+            $channel->addChild('description', 'Catalogo Prodotti '.config('app.name', 'Pubblicittà24'));
 
-        foreach ($products as $product) {
-            $item = $channel->addChild('item');
+            foreach ($products as $product) {
+                $item = $channel->addChild('item');
 
-            // Requisiti Base
-            $item->addChild('g:id', $product->sku, 'http://base.google.com/ns/1.0');
-            $item->addChild('g:title', htmlspecialchars((string) $product->name), 'http://base.google.com/ns/1.0');
-            $item->addChild('g:description', htmlspecialchars((string) $product->plain_description), 'http://base.google.com/ns/1.0');
-            $item->addChild('g:link', route('product', [$product->category?->slug ?? 'uncategorized', $product->slug]), 'http://base.google.com/ns/1.0');
+                // Requisiti Base
+                $item->addChild('g:id', $product->sku, 'http://base.google.com/ns/1.0');
+                $item->addChild('g:title', htmlspecialchars((string) $product->name), 'http://base.google.com/ns/1.0');
+                $item->addChild('g:description', htmlspecialchars((string) $product->plain_description), 'http://base.google.com/ns/1.0');
+                $item->addChild('g:link', route('product', [$product->category?->slug ?? 'uncategorized', $product->slug]), 'http://base.google.com/ns/1.0');
 
-            $imageUrl = $product->getFirstImageUrl('large');
-            $item->addChild('g:image_link', $imageUrl, 'http://base.google.com/ns/1.0');
+                $imageUrl = $product->getFirstImageUrl('large');
+                $item->addChild('g:image_link', $imageUrl, 'http://base.google.com/ns/1.0');
 
-            // Prezzo e Disponibilità
-            $priceData = $product->getDisplayPriceData();
-            $price = number_format((float) $priceData['price'], 2, '.', '');
-            $item->addChild('g:price', $price.' EUR', 'http://base.google.com/ns/1.0');
+                // Prezzo e Disponibilità
+                $priceData = $product->getDisplayPriceData();
+                $price = number_format((float) $priceData['price'], 2, '.', '');
+                $item->addChild('g:price', $price.' EUR', 'http://base.google.com/ns/1.0');
 
-            // Availability
-            $item->addChild('g:availability', 'in_stock', 'http://base.google.com/ns/1.0');
+                // Availability
+                $item->addChild('g:availability', 'in_stock', 'http://base.google.com/ns/1.0');
 
-            // Condizione
-            $item->addChild('g:condition', 'new', 'http://base.google.com/ns/1.0');
+                // Condizione
+                $item->addChild('g:condition', 'new', 'http://base.google.com/ns/1.0');
 
-            // Brand
-            $item->addChild('g:brand', htmlspecialchars((string) $product->brand), 'http://base.google.com/ns/1.0');
+                // Brand
+                $item->addChild('g:brand', htmlspecialchars((string) $product->brand), 'http://base.google.com/ns/1.0');
 
-            // Categoria (opzionale ma consigliato per abbigliamento)
-            $item->addChild('g:product_type', htmlspecialchars((string) ($product->category?->name ?? 'Uncategorized')), 'http://base.google.com/ns/1.0');
+                // Categoria (opzionale ma consigliato per abbigliamento)
+                $item->addChild('g:product_type', htmlspecialchars((string) ($product->category?->name ?? 'Uncategorized')), 'http://base.google.com/ns/1.0');
 
-            // Update time
-            $item->addChild('g:updated_at', Carbon::parse($product->updated_at)->toRfc3339String(), 'http://base.google.com/ns/1.0');
+                // Update time
+                $item->addChild('g:updated_at', Carbon::parse($product->updated_at)->toRfc3339String(), 'http://base.google.com/ns/1.0');
+            }
+
+            $xmlContent = $xml->asXML();
+
+            return response(is_string($xmlContent) ? $xmlContent : '', 200)
+                ->header('Content-Type', 'text/xml');
+        } catch (\Throwable $e) {
+            return response($e->getMessage()."\n".$e->getTraceAsString(), 500)
+                ->header('Content-Type', 'text/plain');
         }
-
-        $xmlContent = $xml->asXML();
-
-        return response(is_string($xmlContent) ? $xmlContent : '', 200)
-            ->header('Content-Type', 'text/xml');
     }
 }
